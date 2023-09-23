186145fd0f36e837e698e5323922ed2618679ad9
KevinDevelopment <kevincristhian@outlook.com>
2023-09-23 09:21:19 -0300

    :sparkles: feat: add prototype reader QR code

--- src/screens/scanner/scanner.tsx
+++ src/screens/scanner/scanner.tsx
@@ -1,17 +1,47 @@
-import { View, Text } from "react-native";
-import { useRoute } from "@react-navigation/native";
-
-type TParamsProps ={
-    name: string
-}
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export function Scanner() {
-    const route = useRoute();
-    const { name } = route.params as TParamsProps;
+  const [hasPermission, setHasPermission] = useState(false);
+  const [scanned, setScanned] = useState(false);
 
-    return (
-        <View style={{ flex: 1, backgroundColor: "blue" }}>
-            <Text>{ name }</Text>
-        </View>
-    );
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(true);
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
