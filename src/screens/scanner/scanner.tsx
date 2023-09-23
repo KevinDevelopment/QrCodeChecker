1d68bae27e5bbc61a3bf6bfb7760184dd5fcb57a
KevinDevelopment <kevincristhian@outlook.com>
2023-09-23 11:28:47 -0300

    :sparkles: feat: add redirect for link in broser

--- src/screens/scanner/scanner.tsx
+++ src/screens/scanner/scanner.tsx
@@ -1,47 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
+import { Linking } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export function Scanner() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [scanned, setScanned] = useState(false);
+    const [hasPermission, setHasPermission] = useState(false);
+    const [scanned, setScanned] = useState(false);
 
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(true);
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(true);
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }: any) => {
+        setScanned(true);
+
+        if (data.startsWith('http://') || data.startsWith('https://')) {
+
+            Linking.openURL(data)
+                .then(() => {
+                    console.log(`Opened link: ${data}`);
+                })
+                .catch((error) => {
+                    console.error(`Error opening link: ${data}`, error);
+                    alert(`Error opening link: ${data}`);
+                });
+        } else {
+            alert(`Bar code with type ${type} and data ${data} is not a link.`);
+        }
     };
 
-    getBarCodeScannerPermissions();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }: any) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
-  return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
 });
